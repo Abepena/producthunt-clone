5b4b7c07ff07d61ac0ffde8bb91a4166fc05fc01
Abe Pena <pena.abe@gmail.com>
2018-10-13 17:06:22 -0400

    editied settings per heroky config

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.storage.CompressedManifestStaticFilesStorage',
 ]
 
 ROOT_URLCONF = 'producthunt.urls'
